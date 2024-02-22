0be7af40d4dd7df63aa15b4ab1e13c588a756d4f
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2024-02-22 11:41:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
-export default function Navbar({value, setValue}) {
-
+export default function Navbar({ value, setValue }) {
   return (
-    <div className="navbar flex items-center justify-between bg-[#010409] px-10">
-      <div className="logo flex items-center py-4 ">
+    <div className="navbar flex items-center justify-between space-x-5 bg-[#010409] px-3 sm:px-10">
+      <div className="logo flex items-center py-5 ">
         <figure>
           <img className="w-10" src="/assets/gitHubLogo.png" alt="Logo" />
         </figure>
         <p className="ml-5">{value}</p>
       </div>
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className="w-[50%] lg:w-[30%] rounded-full py-2 pl-5 text-black"
-        type="text"
-        placeholder="Search by username to get details..."
-      />
+      <div className="flex w-[50%] space-x-1 rounded-full bg-white py-2 px-3 text-black lg:w-[30%]">
+        <SearchIcon className="text-gray-700" />
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="w-full bg-transparent text-black placeholder:text-xs focus:outline-none"
+          type="text"
+          placeholder="Search by username to get details..."
+        />
+      </div>
     </div>
   );
 }
